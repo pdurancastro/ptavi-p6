b265a49a52f0f46d13056b59457b4c7a869e6a67
Patricia Duran Castro <pcastro@gamma05.aulas.gsyc.urjc.es>
2016-11-14 10:42:30 +0100

    Implementando metodos y puerto

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -15,17 +16,35 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+        cliente = self.rfile.read().decode('utf-8').split()
+        #print(cliente)
+        
+        
+        metodo = cliente[0]
+        if metodo == "INVITE":
+        #while 1:
+            #Leyendo línea a línea lo que nos envía el cliente
+            #line = self.rfile.read()
+            print(metodo)
+            #print("El cliente nos manda " + linea.decode('utf-8'))
+        
+        elif metodo == "BYE":
+            print(metodo)
+        
+        elif metodo == "ACK":
+            print(metodo)
+        
+        else:
+            print("NO ES EL METODO CORRECTO")
 
             # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+            #if not line:
+                #break
 
 if __name__ == "__main__":
+    IP = (sys.argv[1])
+    PUERTO = int(sys.argv[2])
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', PUERTO), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
