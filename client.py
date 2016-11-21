0443493c884369ae94acd476b9b44e4112c1fbd0
Patricia Duran Castro <pcastro@gamma05.aulas.gsyc.urjc.es>
2016-11-21 09:42:35 +0100

    cliente con PEP8

--- client.py
+++ client.py
@@ -13,14 +13,14 @@ import sys
 METODO = sys.argv[1]
 DIRECCION = sys.argv[2]
 ListaInfo = DIRECCION.split(':')
-#print(ListaInfo)
+# print(ListaInfo)
 PORT = int(ListaInfo[1])
 SERVIDORTROZ = ListaInfo[0].split('@')
 IP = SERVIDORTROZ[1]
 Cliente = SERVIDORTROZ[0]
-#print(SERVIDORTROZ)
-#print (IP)
-#print (Cliente)
+# print(SERVIDORTROZ)
+# print (IP)
+# print (Cliente)
 
 if len(sys.argv) != 3:
     print("Usage: python client.py method receiver@IP:SIPport")
@@ -39,20 +39,20 @@ print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
-#print('Recibido --\r\n', data.decode('utf-8'))
+# print('Recibido --\r\n', data.decode('utf-8'))
 print(data.decode('utf-8'))
 
 if METODO == 'INVITE':
     Msj_ACK = data.decode('utf-8')
     Msj_ACK_Troz = Msj_ACK.split()
-    
+#   Miro en funcion de si es 100 180 o 200 para mandar el metodo
     Trying = Msj_ACK_Troz[1]
     Ring = Msj_ACK_Troz[4]
-    OK =  Msj_ACK_Troz[7]
-    
-    #ACK DEL INVITE
+    OK = Msj_ACK_Troz[7]
+
+#   ACK DEL INVITE
     if Trying == '100' and Ring == '180' and OK == '200':
-        #print(Msj_ACK_Troz)
+        # print(Msj_ACK_Troz)
         LINE = 'ACK' + ' ' + 'sip:' + Cliente + '@' + IP + ' SIP/2.0'
         my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
         sys.exit()
